d64c413fb35870f4be81055e96b8ee51bb172c63
SebasPask <sebaspask@gmail.com>
2017-11-04 14:31:37 +1100

    simpletest.py has been added to the scripts

--- js/functions.js
+++ js/functions.js
@@ -40,7 +40,7 @@ window.onload = function (){
 	xmlhttp = new XMLHttpRequest();
 	function cameraRight()
 	{
-		xmlhttp.open("GET","cgi-bin/servoMotors/scripts/simpletest.py",true);
+		xmlhttp.open("GET","cgi-bin/simpletest.py",true);
 		xmlhttp.send();
 		alert('done');
 	}
